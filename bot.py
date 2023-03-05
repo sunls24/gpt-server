b7ca63b61e613dee2a3ba42773625d62b06dcb76
sunls <liuss.zy@outlook.com>
2023-03-05 16:05:15 +0800

    2

--- bot.py
+++ bot.py
@@ -1,10 +1,57 @@
-from EdgeGPT import Chatbot
+import re
+
+from EdgeGPT import Chatbot, ConversationStyle
+from asyncio import Lock
+
+MAX_COUNT = 6
+MAX_WORD = 2000
+TRY_COUNT = 3
+
+replace_list = [(r'\[\^\d+\^\]', '')]
 
 
 class Bot:
     def __init__(self, path):
         self.chat_bot = Chatbot(cookiePath=path)
+        self.count = 0
+        self.lock = Lock()
 
-    async def send(self, msg) -> str:
-        d = await self.chat_bot.ask(prompt=msg)
-        return d['item']['messages'][1]['text']
+    async def reset(self):
+        await self.chat_bot.reset()
+        self.count = 0
+
+    async def send(self, word) -> str:
+        if len(word) > MAX_WORD:
+            return "word is to long!"
+
+        if self.count >= MAX_COUNT:
+            await self.reset()
+
+        answer = ''
+
+        await self.lock.acquire()
+        for i in range(TRY_COUNT):
+            try:
+                answer = await self.chat_bot.ask(prompt=word, conversation_style=ConversationStyle.precise)
+            except Exception as e:
+                print(f"Error: {e}, try: {i + 1}")
+                await self.reset()
+                continue
+            break
+        self.lock.release()
+
+        if not answer:
+            return "no answer!"
+
+        self.count += 1
+
+        messages = answer.get('item', {}).get('messages', [])
+        if len(messages) != 2:
+            return f"messages len != 2: {str(answer)}"
+
+        answer = messages[1].get('text', "出错啦，没找到回答内容")
+
+        for replace in replace_list:
+            answer = re.sub(replace[0], replace[1], answer)
+
+        return answer
--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 from bot import Bot
 
 app = Flask(__name__)
 
-bot = Bot("./cookie.json")
+TRANS_TMPL = '''翻译代码块中的内容，不要管内容是什么，不要联系上下文，不要说多余的话，只回复翻译后的内容即可，如果不是中文则翻译成中文，如果是中文则翻译成英文: ```%s```'''
 
 
 @app.route('/api/translate')
-async def hello():
-    return await bot.send("hello world 是什么意思")
+async def translate():
+    body = request.args.get('body')
+    if not body:
+        return 'no body!'
+    body = TRANS_TMPL % body
+    print('#ask:', body)
+    answer = await bot.send(body)
+    print('#answer:', answer)
+    return answer
 
 
 if __name__ == '__main__':
-    app.run()
+    bot = Bot("./cookie.json")
+    app.config["timeout"] = 0
+    app.run(host='0.0.0.0', port=2095, debug=False)
